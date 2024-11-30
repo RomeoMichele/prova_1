e6b9503044bc52d3f989266b0f9986ea400bc38f
ROMEO MICHELE <ROMEO.21280>
2024-11-30 11:59:17 +0100

    cambio1

--- informatica/Form1.cs
+++ informatica/Form1.cs
@@ -6,5 +6,22 @@ namespace informatica
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Console.WriteLine("hello world");
+        }
+
+        public void contatore()
+        { }
+private void button1_Click(object sender, EventArgs e)
+        {
+            cassa1 = new Thread(contatore);
+
+            for(int i=0; i<100;  i++)
+            {
+
+            }
+        }
     }
 }
